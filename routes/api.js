377b83ec0faf8e608926c32385a55922f3086979
sailao <sailaoungseng@gmail.com>
2020-09-27 15:50:23 +0630

    bugfix return error message

--- routes/api.js
+++ routes/api.js
@@ -18,22 +18,19 @@ module.exports = function (app) {
   app.route('/api/convert')
     .get(function (req, res){
       var input = req.query.input;
-      if(!input){
-        return res.json({"error":"invalid number and unit"})
-      }
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       if(initNum == 'invalid number' && initUnit == 'invalid unit'){
-        return res.json({"error":"invalid number and unit"})
+        return res.status(400).json({"error":"invalid number and unit"})
       }
       if(initNum == 'invalid number'){
-        return res.json({"error":"invalid number"})
+        return res.status(400).json({"error":"invalid number"})
       }
       if(initUnit == 'invalid unit'){
-        return res.json({"error":"invalid unit"})
+        return res.status(400).json({"error":"invalid unit"})
       }
       res.json({initNum, initUnit, returnNum, returnUnit, string: toString})
     });
